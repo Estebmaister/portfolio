f716b82900bf7b97a3dc3591b3ba31f2d97fb13f
Esteban Camargo <estebmaister@gmail.com>
2020-05-13 00:41:08 -0400

    :sparkles: Smooth scroll

--- fcc/technicalDocumentation/tech.js
+++ fcc/technicalDocumentation/tech.js
@@ -1,14 +1,29 @@
-// Get the container element
-var btnContainer = document.getElementById("myDIV");
+// selection
+const codes = document.getElementsByClassName("code");
 
-// Get all buttons with class="btn" inside the container
-var btns = btnContainer.getElementsByClassName("btn");
-
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+for (let i = 0; i < codes.length; i++) {
+  codes[i].ondblclick = function (event) {
+    console.log("hello");
+    event.preventDefault();
+    const selection = document.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  };
 }
+
+// // Get the container element
+// var btnContainer = document.getElementById("myDIV");
+
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("btn");
+
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function () {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
